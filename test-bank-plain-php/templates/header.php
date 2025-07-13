1f2e289eb1f413c5e4fc9396161b7ec043809ab8
Janze2022 <janzedoysabas@gmail.com>
2025-07-14 05:00:03 +0800

    Add test-bank-plain-php/templates/header.php 1752440402786

---
+++ test-bank-plain-php/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>PHP Test Bank App</title>
+    <link href="/public/css/style.css" rel="stylesheet" />
+</head>
+<body>
