07f61e816f2d21268042419f966f04b0902bd975
Janze2022 <janzedoysabas@gmail.com>
2025-07-14 05:00:17 +0800

    Add test-bank-plain-php/config/database.php 1752440417319

---
+++ test-bank-plain-php/config/database.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuration for XAMPP MySQL
+return [
+    'host' => '127.0.0.1',
+    'dbname' => 'test_bank_app',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+];
